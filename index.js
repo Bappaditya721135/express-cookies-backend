deef073c4c8d1a820cc2355dc6578f431810c593
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2024-03-24 09:13:42 +0530

    add vercel

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from "express"
+import cors from "cors"
+
+
+const app = express();
+app.use(cors({
+    origin: "https://delicate-sprinkles-11f6af.netlify.app",
+    methods: ["GET", "POST"],
+    credentials: true,
+}))
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "this is the get page"
+    })
+})
+
+
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    if(email === "myemail@gmail.com" && password === "password") {
+        return res.cookie("token",{id: "fejfoefo"}, {
+            // maxAge: 300000,
+            expires: new Date(Date.now() + 300000),
+            sameSite: "none",
+            secure: true,
+        }).json({
+            success: true,
+            message: "login successfull"
+        })
+    }
+    res.json({
+        success: false,
+        message: "invalid credentials"
+    })
+})
+
+
+app.listen(5000, () => {
+    console.log("cors enabled")
+})
